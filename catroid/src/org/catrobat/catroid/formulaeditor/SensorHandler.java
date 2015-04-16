b6f069baf0cac83e427fe1c83623a57623fed19e
Markus Hobisch <hobisch@student.tugraz.at>
2015-04-16 17:49:33 +0200

    DroneConnection & ConfigBrick refactored

--- catroid/src/org/catrobat/catroid/formulaeditor/SensorHandler.java
+++ catroid/src/org/catrobat/catroid/formulaeditor/SensorHandler.java
@@ -28,9 +28,9 @@ import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.util.Log;
 
-import org.catrobat.catroid.drone.DroneInitializer;
+import com.parrot.freeflight.service.DroneControlService;
 
-
+import org.catrobat.catroid.drone.DroneServiceWrapper;
 import org.catrobat.catroid.facedetection.FaceDetectionHandler;
 
 public final class SensorHandler implements SensorEventListener, SensorCustomEventListener {
@@ -108,6 +108,7 @@ public final class SensorHandler implements SensorEventListener, SensorCustomEve
 		if (instance.sensorManager == null) {
 			return 0d;
 		}
+		DroneControlService dcs = DroneServiceWrapper.getInstance().getDroneService();
 		Double sensorValue = 0.0;
 		switch (sensor) {
 
@@ -170,56 +171,55 @@ public final class SensorHandler implements SensorEventListener, SensorCustomEve
 			case LOUDNESS:
 				return Double.valueOf(instance.loudness);
 
-
 			case DRONE_BATTERY_STATUS:
-				return (double)DroneInitializer.droneControlService.getDroneNavData().batteryStatus;
+				return (double)dcs.getDroneNavData().batteryStatus;
 
 			case DRONE_EMERGENCY_STATE:
-				return (double)DroneInitializer.droneControlService.getDroneNavData().emergencyState;
+				return (double)dcs.getDroneNavData().emergencyState;
 
 			case DRONE_USB_REMAINING_TIME:
-				return (double)DroneInitializer.droneControlService.getDroneNavData().usbRemainingTime;
+				return (double)dcs.getDroneNavData().usbRemainingTime;
 
 			case DRONE_NUM_FRAMES:
-				return (double)DroneInitializer.droneControlService.getDroneNavData().numFrames;
+				return (double)dcs.getDroneNavData().numFrames;
 
 			case DRONE_RECORDING:
-				if(DroneInitializer.droneControlService.getDroneNavData().recording){
+				if(dcs.getDroneNavData().recording){
 					return 1d;
 				} else {
 					return 0d;
 				}
 
 			case DRONE_FLYING:
-				if(DroneInitializer.droneControlService.getDroneNavData().flying){
+				if(dcs.getDroneNavData().flying){
 					return 1.0;
 				} else {
 					return 0.0;
 				}
 
 			case DRONE_INITIALIZED:
-				if(DroneInitializer.droneControlService.getDroneNavData().initialized){
+				if(dcs.getDroneNavData().initialized){
 					return 1.0;
 				} else {
 					return 0.0;
 				}
 
 			case DRONE_USB_ACTIVE:
-				if(DroneInitializer.droneControlService.getDroneNavData().usbActive){
+				if(dcs.getDroneNavData().usbActive){
 					return 1.0;
 				} else {
 					return 0.0;
 				}
 
 			case DRONE_CAMERA_READY:
-				if(DroneInitializer.droneControlService.getDroneNavData().cameraReady){
+				if(dcs.getDroneNavData().cameraReady){
 					return 1.0;
 				} else {
 					return 0.0;
 				}
 
 			case DRONE_RECORD_READY:
-				if(DroneInitializer.droneControlService.getDroneNavData().recordReady){
+				if(dcs.getDroneNavData().recordReady){
 					return 1.0;
 				} else {
 					return 0.0;
